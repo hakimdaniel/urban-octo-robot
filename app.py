57ba74d6f3ead225e303b8481567595471d97668
Naga penguasa langit fiksyen <nagamantis@gmail.com>
2025-06-17 21:18:05 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, request
+import telegram
+
+TOKEN = os.getenv("BOT_TOKEN")
+bot = telegram.Bot(token=TOKEN)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Bot aktif di akar root."
+
+@app.route(f"/{TOKEN}", methods=["POST"])
+def webhook():
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+    chat_id = update.message.chat.id
+    text = update.message.text
+
+    reply = f"Salam! Kamu kata: {text}"
+    bot.send_message(chat_id=chat_id, text=reply)
+
+    return 'ok'
