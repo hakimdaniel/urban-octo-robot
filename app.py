8cc1ec1bb380109cd4e234b0bfc985e62cf54e1d
hakimdaniel <mika259123321@gmail.com>
2025-06-17 21:52:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Bot hidup!"
+    return "This bot is running right now."
 
 @app.route(f"/{TOKEN}", methods=["POST"])
 def webhook():
-    update = telegram.Update.de_json(request.get_json(force=True), bot)
-    chat_id = update.message.chat.id
-    text = update.message.text
+    data = request.get_json(force=True)
+    print("DATA DARI TELEGRAM:", data)
 
-    reply = f"Salam Boss! Kamu kata: {text}"
-    bot.send_message(chat_id=chat_id, text=reply)
+    update = telegram.Update.de_json(data, bot)
+
+    if update.message:
+        chat_id = update.message.chat.id
+        text = update.message.text
+        print(f"MSG: {text}")
+
+        reply = f"Salam Boss! Kamu kata: {text}"
+        bot.send_message(chat_id=chat_id, text=reply)
+    else:
+        print("Tiada mesej dalam update")
 
     return 'ok'
